117808e21564fc8c4fa9cdcaa1f602ec22d312f9
MihaiTudorP <mihai.tudor.popescu@gmail.com>
2018-03-25 09:31:02 +0300

    Added more aspect in the index.

---
+++ Prj/ECivis/ECivisWebMVC/wwwroot/js/accordion.js
@@ -0,0 +1,15 @@
+﻿    function toggleIcon(e) {
+ 
+        $(e.target)
+ 
+            .prev('.accordion-heading')
+ 
+            .find(".short-full")
+ 
+            .toggleClass('glyphicon-plus glyphicon-minus');
+ 
+    }
+ 
+    $('.accordion-group').on('hidden.bs.collapse', toggleIcon);
+ 
+    $('.accordion-group').on('shown.bs.collapse', toggleIcon)
